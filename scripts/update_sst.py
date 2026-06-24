552842447b971f804b1dc3a87c9757f4039c246c
alistairas <alistairshutt@gmail.com>
2026-06-24 17:52:05 +0100

    Update update_sst.py

--- scripts/update_sst.py
+++ scripts/update_sst.py
@@ -192,7 +192,7 @@ data = {
     "sample_lon": LON,
     "actual_pixel_lat": actual_lat,
     "actual_pixel_lon": actual_lon,
-    "sample_method": ""sample_method": "Nearest available OSTIA SST grid cell for the North Tyneside coast",
+    "sample_method": "Nearest available OSTIA SST grid cell for the North Tyneside coast",
     "measurement": "Sea temperature",
     "units": "°C",
     "grid_resolution": "0.05 degrees",

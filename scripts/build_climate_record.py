360df73cb4b8aa26dbd45a909f90b7246ae5663f
alistairas <alistairshutt@gmail.com>
2026-07-02 21:57:25 +0100

    Create build_climate_record.py

---
+++ scripts/build_climate_record.py
@@ -0,0 +1,18 @@
+LOCATION_NAME = "North Tyneside coast"
+LOCATION_DESCRIPTION = "Offshore coastal waters around Whitley Bay, Cullercoats and Tynemouth"
+
+# Reference point near Whitley Bay / PSC
+LAT = 55.0519527
+LON = -1.4479198
+
+# Current OSTIA box: small, higher-resolution satellite product
+OSTIA_MIN_LAT = 55.00
+OSTIA_MAX_LAT = 55.09
+OSTIA_MIN_LON = -1.50
+OSTIA_MAX_LON = -1.40
+
+# ERA5 climate box: wider, because ERA5 grid is coarser
+ERA5_NORTH = 55.25
+ERA5_SOUTH = 54.75
+ERA5_WEST = -1.75
+ERA5_EAST = -1.00
